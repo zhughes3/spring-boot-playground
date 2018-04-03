c7940198128387bb06fdbd3637ebcbf89d2f165a
Zachary Hughes <zhughes@captechventures.com>
2018-04-03 13:01:28 -0400

    encapsulated haversine method into own method; added enum to specify which units are being used

--- src/main/java/com/example/springbootplayground/resources/Distance.java
+++ src/main/java/com/example/springbootplayground/resources/Distance.java
@@ -6,18 +6,27 @@ package com.example.springbootplayground.resources;
  *
  */
 public class Distance {
-	double distance;
+	private double distance;
+	final double RADIUS_OF_EARTH = 6372.8; //in kilometers
+	enum Unit {
+		MILES, METERS, KILOMETERS
+	}
+	Unit unit = Unit.MILES;
 	
 	public Distance(double[] srcCoords, double[] destCoords) {
-		setDistance(srcCoords[0], srcCoords[1], destCoords[0], destCoords[1], 0, 0);
+		setDistance(srcCoords[0], srcCoords[1], destCoords[0], destCoords[1]);
 	}
 	
 	public Distance(double srcLat, double srcLong, double destLat, double destLong) {
-		setDistance(srcLat, srcLong, destLat, destLong, 0, 0);
+		setDistance(srcLat, srcLong, destLat, destLong);
 	}
 	
 	public double getDistance() {
 		return this.distance;
+	}
+	
+	public Unit getUnit() {
+		return this.unit;
 	}
 	
 	/**
@@ -31,26 +40,29 @@ public class Distance {
 	 * @param el1
 	 * @param el2
 	 */
-	// https://stackoverflow.com/questions/3694380/calculating-distance-between-two-points-using-latitude-longitude-what-am-i-doi
 	// calculate distance between two lat/long coordinates
-	private void setDistance(double lat1, double lat2, double lon1,
-	        double lon2, double el1, double el2) {
-
-	    final int R = 6371; // Radius of the earth
-
-	    double latDistance = Math.toRadians(lat2 - lat1);
-	    double lonDistance = Math.toRadians(lon2 - lon1);
-	    double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-	            + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-	            * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-	    double distance = R * c * 1000; // convert to meters
-
-	    double height = el1 - el2;
-
-	    distance = Math.pow(distance, 2) + Math.pow(height, 2);
-
-	    this.distance = Math.sqrt(distance);
+	private void setDistance(double srcLat, double srcLong, double destLat, double destLong) {
+	   distance = haversine(srcLat, srcLong, destLat, destLong);
+	}
+	
+	private double haversine(double lat1, double long1, double lat2, double long2) {
+		double dLat = Math.toRadians(lat2-lat1);
+		double dLong = Math.toRadians(long2 - long1);
+		
+		lat1 = Math.toRadians(lat1);
+		lat2 = Math.toRadians(lat2);
+		
+		double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLong / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+		double c = 2 * Math.asin(Math.sqrt(a));
+		if (unit == Unit.MILES) {
+			return (RADIUS_OF_EARTH * c)/1.609344;
+		} else if (unit == Unit.METERS) {
+			return RADIUS_OF_EARTH * c * 1000;
+		} else if (unit == Unit.KILOMETERS) {
+			return RADIUS_OF_EARTH * c;
+		}
+		
+		return 0.0;
 	}
 
 }
